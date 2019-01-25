c84b0b497131efc3cfb4ab570085ccc95996759c
Clément SIBILLE <claymeuns@protonmail.com>
2019-01-25 22:09:30 +0100

    Add usage example for tuber_window_sdl2

---
+++ src/bin/example.rs
@@ -0,0 +1,43 @@
+/*
+* MIT License
+*
+* Copyright (c) 2018 Clément SIBILLE
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+extern crate tuber_window_sdl2;
+extern crate tuber;
+
+use tuber::window::Window;
+use tuber::window::WindowEvent;
+use tuber_window_sdl2::SDLWindow;
+
+
+fn main() {
+    let mut window = SDLWindow::create("ExampleWindow", 800u32, 600u32).unwrap();
+
+    loop {
+        let event = window.poll_event();
+
+        if let WindowEvent::Close = event {
+            break;
+        }
+    }
+}
